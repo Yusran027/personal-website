017488caf7bf6772e807d34093920997ac8ee3b5
Yusran027 <yusranhaliq.l@gmail.com>
2024-07-31 16:34:49 +0800

    menambahkan tombol

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
+// Navbar
+
 // Ambil elemen navbar
 var navbar = document.getElementById('navbar');
 var lastScrollTop = 0;
 
 // Fungsi untuk menentukan apakah scroll ke atas atau ke bawah
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', function () {
   var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
   if (scrollTop > lastScrollTop) {
@@ -16,3 +18,21 @@ window.addEventListener('scroll', function() {
 
   lastScrollTop = scrollTop;
 });
+
+
+// Tombol Splash Buble
+var animateButton = function (e) {
+  e.preventDefault;
+  //reset animation
+  e.target.classList.remove('animate');
+
+  e.target.classList.add('animate');
+  setTimeout(function () {
+    e.target.classList.remove('animate');
+  }, 700);
+};
+
+var bubblyButtons = document.getElementsByClassName('splash-button');
+for (var i = 0; i < bubblyButtons.length; i++) {
+  bubblyButtons[i].addEventListener('click', animateButton, false);
+}
