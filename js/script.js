585a52e71718dbc1671901e78cf5cc3727edc760
Yusran027 <yusranhaliq.l@gmail.com>
2024-08-06 13:28:40 +0800

    membuat navbar mobile

--- js/script.js
+++ js/script.js
@@ -1,23 +1,54 @@
-// Navbar
+// Navbar-fungsi cadangan
 
-// Ambil elemen navbar
-var navbar = document.getElementById('navbar');
-var lastScrollTop = 0;
 
 // Fungsi untuk menentukan apakah scroll ke atas atau ke bawah
-window.addEventListener('scroll', function () {
-  var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+// Ambil elemen navbar
+// var navbar = document.getElementById('navbar');
+// var lastScrollTop = 0;
+// window.addEventListener('scroll', function () {
+//   var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
-  if (scrollTop > lastScrollTop) {
-    // Scroll ke bawah
-    navbar.classList.remove('navbar-show');
-  } else {
-    // Scroll ke atas
-    navbar.classList.add('navbar-show');
+//   if (scrollTop > lastScrollTop) {
+   
+//     navbar.classList.remove('navbar-show');
+//   } else {
+    
+//     navbar.classList.add('navbar-show');
+//   }
+
+//   lastScrollTop = scrollTop;
+// });
+
+//Toggle Class Active untuk hamburger menu
+
+const navbarNav = document.querySelector('.navbar-nav');
+// ketika hamburger diklik
+
+document.querySelector('#hamburger-menu').onclick = (e) => {
+  navbarNav.classList.toggle('active');
+  e.preventDefault(); // Hanya diperlukan jika Anda ingin menghentikan perilaku default dari tombol pencarian
+};
+
+// klik diluar sidebar untuk menghilangkan nav
+const hm = document.querySelector('#hamburger-menu');
+const sb = document.querySelector('#search-button');
+const scb = document.querySelector('#shopping-cart-button');
+
+document.addEventListener('click', function (e) {
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove('active');
   }
-
-  lastScrollTop = scrollTop;
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove('active');
+  }
+  if (!scb.contains(e.target) && !searchForm.contains(e.target)) {
+    shoppingCart.classList.remove('active');
+  }
 });
+
+
+
+
 
 // Tombol Splash Buble
 var animateButton = function (e) {
